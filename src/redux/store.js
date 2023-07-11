af3d04bb7ac27653f0beba4415634b12c7a55400
NataliaNazarenko <115888204+NataliaNazarenko@users.noreply.github.com>
2023-07-11 23:57:51 +0300

    test

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage';
 import {
   persistStore,
