5e802afc76b09f37f656304fa58358d5b3fcfebf
NataliaNazarenko <115888204+NataliaNazarenko@users.noreply.github.com>
2023-07-16 22:01:36 +0300

    fixed bug

--- src/components/UserMenu/UserMenu.styled.jsx
+++ src/components/UserMenu/UserMenu.styled.jsx
@@ -1,31 +1,31 @@
 import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 12px;
+display: flex;
+align-items: center;
+gap: ${props => props.theme.spacing(3)}
 `;
 
 export const Text = styled.p`
-  font-weight: 700;
-  color: #f08080;
-  font-size: 20px;
+font-weight: 700;
+color: ${props => props.theme.colors.accent};
+font-size: ${props => props.theme.fontSizes.large}
 `;
 
 export const Button = styled.button`
-  padding: 4px 6px;
-  font: inherit;
-  cursor: pointer;
-  border-radius: 4px 8px;
-  border: 3px solid #f08080;
-  color: #f08080;
-  font-size: 20px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: 10px 10px;
+padding: ${props => props.theme.spacing(1)} ${props => props.theme.spacing(2)};
+font: inherit;
+cursor: pointer;
+border-radius: ${props => props.theme.spacing(1)} ${props => props.theme.spacing(2)};
+border: 3px solid ${props => props.theme.colors.accent};
+color: ${props => props.theme.colors.accent};
+font-size: ${props => props.theme.fontSizes.large};
+display: flex;
+align-items: center;
+justify-content: center;
+margin: ${props => props.theme.spacing(2)} ${props => props.theme.spacing(2)};
 
-  &:hover {
-    background-color: #b9bcd3;
-  }
+&:hover {
+background-color: ${props => props.theme.colors.light};
+}
 `;
--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Global styles={GlobalStyles} />
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <BrowserRouter>
+          <BrowserRouter basename="/goit-react-hw-08-phonebook">
             <App />
           </BrowserRouter>
         </PersistGate>
